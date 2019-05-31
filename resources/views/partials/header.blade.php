2c5c4754601ebdfb09eeb20f63699838c2808cde
giorgioelrey <petetti.giorgio@gmail.com>
2019-05-31 19:32:27 +0200

    dashboard create pages nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,13 @@
-questo è header
+<div class="container-fluid p-0 position-sticky">
+<div class="row m-0">
+<div class="col-md-12 p-0">
+<nav class="navbar navbar-dark fixed-top bg-white flex-md-nowrap p-0 shadow">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#"><img src="{{ asset('storage/img/logo_core.png') }}" alt="core"  width="200" height="auto"></a>
+</a>
+      
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <a class="nav-link text-danger" href="#">Sign out</a>
+        </li>
+      </ul>
+    </nav>
