4ba6d759e27b5dc42053181ffc7b3c94daa916ac
giorgioelrey <petetti.giorgio@gmail.com>
2019-06-15 19:49:48 +0200

    con title

--- resources/views/frontend/pages/press.blade.php
+++ resources/views/frontend/pages/press.blade.php
@@ -3,11 +3,33 @@
 
 
 <div class='container ' id='news'>
+  <div  class='title'>Press
+
+  <div class='sotto_title'>
+
+  </div>
+  </div>
+  <div class='row mt-5'>
 
 
 
+<div class='col-md-4'>
+  ytest
+</div>
+
+<div class='col-md-4'>
+    ytest
+</div>
+
+<div class='col-md-4'>
+    ytest
+</div>
 
 </div>
+</div>
+
+
+
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -21,26 +21,32 @@ Route::view('/admin/{path?}', 'backend.layouts.app');
 Route::get('/', function () {
     return view('frontend.pages.home');
 });
-Route::get('/chi siamo', function () {
-    return view('frontend.pages.chi siamo');
-});
+Route::get('/chi-siamo', function () {
+    return view('frontend.pages.chi-siamo');
+})->name('fe.chi-siamo');
+
 Route::get('/soci', function () {
     return view('frontend.pages.soci');
-});
+})->name('fe.soci');
+
 Route::get('/eventi', function () {
     return view('frontend.pages.eventi');
-});
+})->name('fe.eventi');
+
 Route::get('/servizi-mnlounge', function () {
     return view('frontend.pages.servizi-mnlounge');
-});
+})->name('fe.servizi-mnlounge');
+
 Route::get('/press', function () {
     return view('frontend.pages.press');
-});
+})->name('fe.press');
+
 Route::get('/archivio-storico', function () {
     return view('frontend.pages.archivio-storico');
-});
+})->name('fe.archivio-storico');
+
 Route::get('/contatti', function () {
     return view('frontend.pages.contatti');
-});
+})->name('fe.contatti');
 
 Auth::routes();
