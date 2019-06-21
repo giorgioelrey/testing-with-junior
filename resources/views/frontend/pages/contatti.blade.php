88d72ada551ab05f78a3cead99d01a92a0c08305
giorgioelrey <petetti.giorgio@gmail.com>
2019-06-21 17:54:55 +0200

    calenda skinnato pagine post skinnate

--- resources/views/frontend/pages/contatti.blade.php
+++ resources/views/frontend/pages/contatti.blade.php
@@ -5,36 +5,8 @@
 <div class='container-fluid  ' id='contatti'>
 <div class='row'>
 
-<div class='col-md-6 bg-dark'>
+<div class='col-md-6 bg-dark vh-100'>
 
-
-
-</div>
-
-
-
-
-
-
-
-<div class='col-md-6'>
-<h1 class='title text-center'>contatti</h1>
-<div class='pl-5'>
-<p>Address</p>
-<p>Via dei Billighi 26, 20100 Milano</p>
-
-
-
-<p>Email</p>
-<p><a href='mailto:segreteria@montenapoleonedistrict.it'> segreteria@montenapoleonedistrict.it</a>
-</p>
-
-
-
-<p>Telefono</p>
-<p>02 2035445566
-</p>
-</div>
 
 
 </div>
@@ -46,21 +18,54 @@
 
 
 <div class='col-md-6 '>
+  <div  class='title'>Contatti
+
+    <div class='sotto_title'>
+
+    </div>
+  </div>
+<div class='pl-5 rif-contatti'>
+<p>Address:<br>
+Via dei Billighi 26, 20100 Milano</p>
+
+
+
+<p>Email:<br>
+<a href='mailto:segreteria@montenapoleonedistrict.it'> segreteria@montenapoleonedistrict.it</a>
+</p>
+
+
+
+<p>Telefono:<br>
+02 2035445566
+</p>
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+<div class='col-md-6 d-flex align-items-center'>
   <form>
     <div class="form-group">
       <label for="formGroupExampleInput">Nome</label>
-    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Nome">
+
     </div>
     <div class="form-group">
       <label for="formGroupExampleInput2">Cognome</label>
-     <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+     <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Cognome">
     </div>
     <div class="form-group">
     <label for="exampleFormControlTextarea1">Messaggio</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" cols="50"></textarea>
   </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary invia">Invia</button>
   </form>
 
 
@@ -72,7 +77,7 @@
 
 
 
-<div class='col-md-6 bg-dark'>
+<div class='col-md-6 bg-dark vh-100'>
 
 </div>
 
