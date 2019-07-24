dd6ff926afb3d6a2f39fc4beef43f74d5b55bee7
silvicardo <silvicardo@gmail.com>
2019-07-24 22:47:41 +0200

    frontend fix to events

--- app/Http/Controllers/frontendApi/EventController.php
+++ app/Http/Controllers/frontendApi/EventController.php
@@ -23,9 +23,9 @@ class EventController extends Controller
             'events' => Event::all()->each(function ($item, $key) {
 
              //Check if is a loremPixel url otherwise get url for img tag
-              $urlSplit = explode("/",$item['image_url']);
+              $urlSplit = explode("/",$item->image_url);
               if (!in_array('lorempixel.com', $urlSplit)){
-               $item['image_url'] = Storage::url($item['image_url']);
+               $item->image_url = Storage::url($item->image_url);
              }
 
              })->toArray(),
