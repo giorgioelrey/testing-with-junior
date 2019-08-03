0d0f3681de73f475f94220e3fdcc665d8b81805d
silvicardo <silvicardo@gmail.com>
2019-08-03 19:39:48 +0200

    enabling image in editor

--- resources/js/backend/react/components/dashboard/forms/WisiwygEditorFormikField.js
+++ resources/js/backend/react/components/dashboard/forms/WisiwygEditorFormikField.js
@@ -10,7 +10,27 @@ const WisiwygEditorFormikField = ({ errors, name, label}) => (
      {({ field, errors }) =>
      {
        //console.log('wisiwyg field', field,  'errors', errors);
-       return <ReactQuill value={field.value} onChange={field.onChange(field.name)} />
+       return <ReactQuill
+         value={field.value}
+        onChange={field.onChange(field.name)}
+        modules = {{
+  toolbar: [
+    [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+    [{size: []}],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{'list': 'ordered'}, {'list': 'bullet'},
+     {'indent': '-1'}, {'indent': '+1'}],
+    ['link', 'image', 'video'],
+    ['clean']
+  ]
+}}
+        formats ={ [
+                      'header', 'font', 'size',
+                      'bold', 'italic', 'underline', 'strike', 'blockquote',
+                      'list', 'bullet', 'indent',
+                      'link', 'image', 'video'
+                    ]}
+/>
      }}
      </Field>
      <div className={'invalid-feedback ' + (errors[name] ? 'd-block' : '')}>{errors[name]}</div>
