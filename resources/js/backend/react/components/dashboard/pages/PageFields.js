3fccb05db2fa011fda9bd6d25c11d4badd9c9eb0
silvicardo <silvicardo@gmail.com>
2019-09-06 22:55:09 +0200

    pages home vendemmia updates

--- resources/js/backend/react/components/dashboard/pages/PageFields.js
+++ resources/js/backend/react/components/dashboard/pages/PageFields.js
@@ -1,30 +1,58 @@
 import React, {Fragment} from 'react';
-import { Field, ErrorMessage } from 'formik';
+import {Field, ErrorMessage, Form} from 'formik';
 import TextInputPageField from './TextInputPageField';
 import WisiwygEditorPageField from './WisiwygEditorPageField';
 import FileUploadInputFormikField from './../forms/FileUploadInputFormikField';
 
-const PageFields = ({ errors, status, touched, fieldsData, setFieldValue, values }) => {
+const PageFields = ({ errors, status, touched, fieldsData, setFieldValue, values, layout }) => {
 
-  return (
-    <Fragment>
-    { Object
-         .values(fieldsData)
-         .map((field, idx) => {
+    const fieldRenderer = (field, idx) => {
 
-          switch (field.type) {
+        switch (field.type) {
 
             case 'string': return (<TextInputPageField key={idx} errors={errors} name={field.name} touched={touched} />);break;
 
             case 'wisiwyg': return (<WisiwygEditorPageField key={idx} errors={errors} field={field} />); break;
 
-            case 'image': console.log('field image ', field.name); return (<FileUploadInputFormikField key={idx} setFieldValue={setFieldValue} label={field.name} name={field.name} values={values} errors={errors} touched={touched} currentImage={field.previousUrl}/>); break;
+            case 'image': return (<FileUploadInputFormikField key={idx} setFieldValue={setFieldValue} label={field.name} name={field.name} values={values} errors={errors} touched={touched} currentImage={field.previousUrl}/>); break;
 
             default: break;
-          }
-
-          })
         }
+
+    };
+
+    const sectionRenderer = (section, idx) => {
+
+        const fields = [];
+
+        section.fields.forEach((fieldName, idx) => {
+            //console.log('sectionrenderfield',fieldName, fieldsData[fieldName]);
+            fields.push(fieldRenderer(fieldsData[fieldName], idx));
+        });
+
+
+        return (
+            <div className="my-5" key={idx}>
+
+                <h2>{section.title}</h2>
+
+                {fields}
+
+                <hr/>
+
+            </div>
+        )
+    };
+
+    const unorderedRenderedForm =  Object.values(fieldsData).map(fieldRenderer);
+
+    const sections = layout && JSON.parse(layout).sections || null;
+
+    console.log('sections',sections);
+
+    return (
+    <Fragment>
+        {sections != null && sections.map(sectionRenderer) || unorderedRenderedForm }
     </Fragment>
   );
 }
