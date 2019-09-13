fccc6f03a11e18383191823a619bc8018f0821ab
giorgioelrey <petetti.giorgio@gmail.com>
2019-09-13 16:27:01 +0200

    'cssvari-info-footer-lang'

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -1,11 +1,14 @@
 
     <!-- Footer -->
     <footer class="" id='footer'>
+
       <div id="quadrato_top">
 <div id="top"></div>
 </div>
+<p class="info">MONTENAPOLEONE DISTRICT -  Via Bigli 24 20121 Milano -  P.I. 07518050963</p>
       <!-- Copyright -->
       <div class="footer-copyright text-right mr-5">
+
         <a href="https://www.google.com"><i class="fab fa-weixin"></i></a>
           <a href="https://www.google.com"><i class="fab fa-facebook-f"></i></a>
         <a href="https://www.instagram.com/montenapoleonedistrict/">  <i class="fab fa-instagram"></i></a>
