2d1d76626913bec39786d71ddbd742de5903639c
silvicardo <silvicardo@gmail.com>
2020-09-29 18:42:49 +0200

    pdf privacy

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -5,13 +5,18 @@
       <div id="quadrato_top">
 <div id="top"></div>
 </div>
-<p class="info">MONTENAPOLEONE DISTRICT -  Via Bigli 24 20121 Milano -  P.I. 07518050963</p>
+        <p class="info">MONTENAPOLEONE DISTRICT -  Via Bigli 24 20121 Milano -  P.I. 07518050963
+            {{--
+
+            <a href="">Privacy Policy</a><a href="">Privacy Newsletter</a></p>
+            <iframe src="http://docs.google.com/gview?url=http://example.com/mypdf.pdf&embedded=true" style="width:718px; height:700px;" frameborder="0"></iframe>
+            --}}
       <!-- Copyright -->
       <div class="footer-copyright text-right mr-5">
 
       {{-- <a href="https://www.google.com"><i class="fab fa-weixin"></i></a> --}}
           <a href="https://www.google.com"><i class="fab fa-facebook-f"></i></a>
-        <a href="https://www.instagram.com/montenapoleonedistrict/">  <i class="fab fa-instagram"></i></a>
+          <a href="https://www.instagram.com/montenapoleonedistrict/">  <i class="fab fa-instagram"></i></a>
       </div>
       <!-- Copyright -->
 
