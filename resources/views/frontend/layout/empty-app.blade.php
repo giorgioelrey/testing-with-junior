1b631cb8047207804a2dc4c3b5d73da4c45c2b16
silvicardo <silvicardo@gmail.com>
2021-05-08 09:19:42 +0200

    .idea folder in gitignore

---
+++ resources/views/frontend/layout/empty-app.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="format-detection" content="telephone=no">
+  @yield('meta-description')
+
+  <script src="https://kit.fontawesome.com/71611066a6.js"></script>
+
+  @php
+  $currentRouteName =  Route::currentRouteName();
+
+  $detectedLocale = App::getLocale();
+
+  $detectedLocaleIsIt = App::isLocale('it') ? true : false;
+
+  //$lang gets received by route function
+
+  @endphp
+  {{-- Passing to js userLanguage--}}
+  <script type="text/javascript">
+    const userLanguage = "{{$lang}}";
+  </script>
+
+  {{-- Passing to js gmapsApiKey--}}
+  <script type="text/javascript">
+    const gmapsApiKey = "{{config('app.gmaps_api_key')}}";
+  </script>
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  @yield('title')
+
+  <!-- Scripts -->
+    @yield('header_scripts')
+  <script src="{{ asset('js/frontend/app.js') }}" defer></script>
+
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+
+
+  <!-- Styles -->
+  <link href="{{ asset('css/frontend.css') }}" rel="stylesheet">
+
+  @yield('css')
+
+</head>
+
+<body>
+
+  @include('frontend.partials.navbar')
+
+  <main>
+
+    @yield('content')
+
+  </main>
+  @yield('scripts')
+
+  @include('frontend.partials.footer')
+
+</body>
+</html>
